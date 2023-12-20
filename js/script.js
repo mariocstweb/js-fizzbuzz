8b429f1ea920bd61c8f3586f8883e5ed1d014f50
mariocstweb <mariocstmra@gmail.com>
2023-12-20 15:30:31 +0100

    added js content

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("js ok");
+// console.log("js ok");
+
+// 1) Stampiamo in console un programma che generi i numeri da 1 a 100
+// 2 Controlliamo se i numeri siano multipli di
+//   3 5 o entrambi
+
+for (let i = 1; i <= 100; i++) {
+  // console.log(i);
+  if (i % 3 === 0 && i % 5 === 0) {
+    // console.log(i);
+  } else if (i % 5 === 0) {
+    // console.log(i);
+  } else if (i % 3 === 0) {
+    console.log(i);
+  }
+}
